03aa74ff94c8068724a20ac63c23ce36bddc4eda
Ice Sarunpawat <sarunpawat.p@ku.th>
2022-08-03 15:27:08 +0700

    Add controllers/views

---
+++ src/main/java/ku/cs/controllers/ShowUserDetailController.java
@@ -0,0 +1,14 @@
+package ku.cs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ShowUserDetailController {
+    @FXML
+    private TableView<String> userTable;
+
+    @FXML
+    public void handdleBackButton(){
+
+    }
+}
