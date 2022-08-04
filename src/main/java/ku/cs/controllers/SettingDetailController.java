1040f42ae3378ed08f57762e4bde67d284d2d898
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2022-08-04 22:20:43 +0700

    Design UI Dashboard and Constructor

--- src/main/java/ku/cs/controllers/SettingDetailController.java
+++ src/main/java/ku/cs/controllers/SettingDetailController.java
@@ -1,4 +1,18 @@
 package ku.cs.controllers;
 
+import javafx.fxml.FXML;
+
 public class SettingDetailController {
+    @FXML private void handleChangeName (){
+
+    }
+    @FXML private void handleChangeProfile (){
+
+    }
+    @FXML private void handleChangePassword (){
+
+    }
+    @FXML private void handleChangeTheme (){
+
+    }
 }
