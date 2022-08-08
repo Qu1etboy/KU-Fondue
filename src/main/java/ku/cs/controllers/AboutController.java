5c68fed167ef7dc012e910f1b91bfed389cd8486
Qu1etboy <nonzagreanthai@gmail.com>
2022-08-08 20:03:45 +0700

    Combind view to MainApplication

--- src/main/java/ku/cs/controllers/AboutController.java
+++ src/main/java/ku/cs/controllers/AboutController.java
@@ -1,8 +1,14 @@
 package ku.cs.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class AboutController {
     @FXML private ImageView image1;
@@ -12,20 +18,24 @@ public class AboutController {
 
     @FXML
     public void initialize(){
-        String ong = getClass().getResource("/ku/cs/images/ong.jpg").toExternalForm();
-        image1.setImage(new Image(ong));
-        String arm = getClass().getResource("/ku/cs/images/arm.jpg").toExternalForm();
-        image2.setImage(new Image(arm));
-        String non = getClass().getResource("/ku/cs/images/non.jpg").toExternalForm();
-        image3.setImage(new Image(non));
-        String ice = getClass().getResource("/ku/cs/images/ice.jpg").toExternalForm();
-        image4.setImage(new Image(ice));
+//        String ong = getClass().getResource("/ku/cs/images/ong.jpg").toExternalForm();
+//        image1.setImage(new Image(ong));
+//        String arm = getClass().getResource("/ku/cs/images/arm.jpg").toExternalForm();
+//        image2.setImage(new Image(arm));
+//        String non = getClass().getResource("/ku/cs/images/non.jpg").toExternalForm();
+//        image3.setImage(new Image(non));
+//        String ice = getClass().getResource("/ku/cs/images/ice.jpg").toExternalForm();
+//        image4.setImage(new Image(ice));
     }
 
-    @FXML private void handleNextButton (){
-
+    @FXML private void handleNextButton (ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/about2.fxml"));
+        BorderPane borderPane = (BorderPane) ((Node) actionEvent.getSource()).getScene().getRoot();
+        borderPane.setCenter(loader.load());
     }
-    @FXML private void handlePreviousButton (){
-
+    @FXML private void handlePreviousButton (ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/about1.fxml"));
+        BorderPane borderPane = (BorderPane) ((Node) actionEvent.getSource()).getScene().getRoot();
+        borderPane.setCenter(loader.load());
     }
 }
--- src/main/java/ku/cs/controllers/MainApplicationController.java
+++ src/main/java/ku/cs/controllers/MainApplicationController.java
@@ -1,8 +1,11 @@
 package ku.cs.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainApplicationController {
     @FXML private BorderPane borderPane;
@@ -20,48 +23,63 @@ public class MainApplicationController {
         borderPane.getStylesheets().add(themeCSS);
     }
     @FXML
-    public void handleHomeButton() {
+    public void handleHomeButton() throws IOException {
         homeButton.getStyleClass().add("active");
         dashboardButton.getStyleClass().remove("active");
         aboutButton.getStyleClass().remove("active");
         helpButton.getStyleClass().remove("active");
         settingButton.getStyleClass().remove("active");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/home.fxml"));
+        borderPane.setCenter(loader.load());
     }
 
     @FXML
-    public void handleDashboardButton() {
+    public void handleDashboardButton() throws IOException {
         homeButton.getStyleClass().remove("active");
         dashboardButton.getStyleClass().add("active");
         aboutButton.getStyleClass().remove("active");
         helpButton.getStyleClass().remove("active");
         settingButton.getStyleClass().remove("active");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/adminDashboard.fxml"));
+        borderPane.setCenter(loader.load());
     }
 
     @FXML
-    public void handleAboutButton() {
+    public void handleAboutButton() throws IOException {
         homeButton.getStyleClass().remove("active");
         dashboardButton.getStyleClass().remove("active");
         aboutButton.getStyleClass().add("active");
         helpButton.getStyleClass().remove("active");
         settingButton.getStyleClass().remove("active");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/about1.fxml"));
+        borderPane.setCenter(loader.load());
     }
 
     @FXML
-    public void handleHelpButton() {
+    public void handleHelpButton() throws IOException{
         homeButton.getStyleClass().remove("active");
         dashboardButton.getStyleClass().remove("active");
         aboutButton.getStyleClass().remove("active");
         helpButton.getStyleClass().add("active");
         settingButton.getStyleClass().remove("active");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/help.fxml"));
+        borderPane.setCenter(loader.load());
     }
 
     @FXML
-    public void handleSettingButton() {
+    public void handleSettingButton() throws IOException{
         homeButton.getStyleClass().remove("active");
         dashboardButton.getStyleClass().remove("active");
         aboutButton.getStyleClass().remove("active");
         helpButton.getStyleClass().remove("active");
         settingButton.getStyleClass().add("active");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/setting.fxml"));
+        borderPane.setCenter(loader.load());
     }
 
 
