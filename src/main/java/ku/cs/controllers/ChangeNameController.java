7250acb914973fe0a77cd85c078020087306b954
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2022-09-04 00:34:58 +0700

    create ChangeName,ChangePassword Controller

--- src/main/java/ku/cs/controllers/ChangeNameController.java
+++ src/main/java/ku/cs/controllers/ChangeNameController.java
@@ -1,2 +1,91 @@
-package ku.cs.controllers;public class ChangeNameController {
+package ku.cs.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import ku.cs.models.Appearance;
+import ku.cs.models.Login;
+import ku.cs.models.User;
+import ku.cs.models.UserList;
+import ku.cs.services.DataSource;
+import ku.cs.services.UserListDataSource;
+
+import java.io.IOException;
+
+public class ChangeNameController {
+    private User user;
+    private UserList userList;
+    private DataSource<UserList> data;
+    private Login login;
+    @FXML
+    private Label usernameLabel;
+    @FXML private TextField userNameTextField;
+    @FXML private TextField passwordPasswordField;
+    @FXML private TextField userNameNewTextField;
+
+    public void initData(User user) {
+        this.user = user;
+        data = new UserListDataSource("data", "user.csv");
+        userList = data.readData();
+        login = new Login(userList);
+    }
+
+    @FXML private void handleOKButton(ActionEvent actionEvent) throws IOException {
+        String username = userNameTextField.getText();
+        String password = passwordPasswordField.getText();
+        String usernameNew = userNameNewTextField.getText();
+        User user = login.checkUsername(username);
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please fill in all the field");
+            alert.show();
+            return;
+        }
+        if (user == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("User doesn't exist");
+            alert.show();
+            return;
+        }
+        if (!login.checkPassword(user.getPassword(), password)) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Password is incorrect");
+            alert.show();
+            return;
+        }
+
+        user.setName(usernameNew);
+        userList.updateUser(user);
+        data.writeData(userList);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/setting.fxml"));
+        BorderPane borderPane = (BorderPane) ((StackPane) ((Node) actionEvent.getSource()).getScene().getRoot()).getChildren().get(0);
+        Parent pane = loader.load();
+
+        // sent data to setting controller
+        SettingDetailController settingDetailController = loader.getController();
+        settingDetailController.initData(user);
+
+        borderPane.setCenter(pane);
+    }
+
+    @FXML private void handleCancelButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/setting.fxml"));
+        BorderPane borderPane = (BorderPane) ((StackPane) ((Node) actionEvent.getSource()).getScene().getRoot()).getChildren().get(0);
+        Parent pane = loader.load();
+
+        // sent data to setting controller
+        SettingDetailController settingDetailController = loader.getController();
+        settingDetailController.initData(user);
+
+        borderPane.setCenter(pane);
+    }
 }
--- src/main/java/ku/cs/controllers/SettingDetailController.java
+++ src/main/java/ku/cs/controllers/SettingDetailController.java
@@ -10,6 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -31,6 +32,7 @@ public class SettingDetailController {
     private DataSource<UserList> data;
     private Appearance appearance;
     @FXML private Label usernameLabel;
+    @FXML private Label nameLabel;
     @FXML private ComboBox<String> themeSelector;
 
     @FXML private ComboBox<String> fontSelector;
@@ -40,6 +42,7 @@ public class SettingDetailController {
     public void initData(User user) {
         this.user = user;
         usernameLabel.setText(user.getUsername());
+        nameLabel.setText(user.getName());
 
         data = new UserListDataSource("data", "user.csv");
         userList = data.readData();
@@ -52,7 +55,7 @@ public class SettingDetailController {
         fontSizeSelector.getItems().addAll(appearance.getAllFontSize());
         fontSizeSelector.setOnAction(e -> handleChangFontSize(e));
     }
-
+/*
     @FXML private void handleChangeName(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/changeNameDialog.fxml"));
         Parent root = loader.load();
@@ -112,67 +115,26 @@ public class SettingDetailController {
 
         vBox.setVisible(false);
     }
+
+ */
+    @FXML private void handleChangeName(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/changeNameDialog.fxml"));
+        BorderPane borderPane = (BorderPane) ((StackPane) ((Node) actionEvent.getSource()).getScene().getRoot()).getChildren().get(0);
+        Parent pane = loader.load();
+        ChangeNameController changeNameController = loader.getController();
+        changeNameController.initData(user);
+        borderPane.setCenter(pane);
+    }
     @FXML private void handleChangeProfile (){
 
     }
     @FXML private void handleChangePassword (ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/changePasswordDialog.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-
-        String themeCSS = this.getClass().getResource("/ku/cs/css/themes/" + user.getTheme() + ".css").toExternalForm();
-        String fontCSS = this.getClass().getResource("/ku/cs/css/fonts/" + user.getFont() + ".css").toExternalForm();
-        root.getStylesheets().add(themeCSS);
-        root.getStylesheets().add(fontCSS);
-
-        // TODO: send data to changeNameDialog
-
-        Stage parentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        VBox vBox = (VBox) ((StackPane) ((Node) actionEvent.getSource()).getScene().getRoot()).getChildren().get(1);
-
-        Stage dialogBox = new Stage();
-
-        dialogBox.initModality(Modality.APPLICATION_MODAL);
-        dialogBox.initOwner(parentStage);
-        dialogBox.initStyle(StageStyle.TRANSPARENT);
-        scene.setFill(Color.TRANSPARENT);
-
-        dialogBox.setScene(scene);
-
-        // make dialog box close when click outside
-        dialogBox.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
-            if (!isNowFocused) {
-                dialogBox.close();
-            }
-        });
-
-        // make dialog box always center to the screen of application
-        ChangeListener<Number> widthListener = (observable, oldValue, newValue) -> {
-            double stageWidth = newValue.doubleValue();
-            dialogBox.setX(parentStage.getX() + parentStage.getWidth() / 2 - stageWidth / 2);
-        };
-        ChangeListener<Number> heightListener = (observable, oldValue, newValue) -> {
-            double stageHeight = newValue.doubleValue();
-            dialogBox.setY(parentStage.getY() + parentStage.getHeight() / 2 - stageHeight / 2);
-        };
-        dialogBox.widthProperty().addListener(widthListener);
-        dialogBox.heightProperty().addListener(heightListener);
-        dialogBox.setOnShown(e -> {
-            dialogBox.widthProperty().removeListener(widthListener);
-            dialogBox.heightProperty().removeListener(heightListener);
-        });
-
-        // make background of parent stage darker
-        vBox.setVisible(true);
-        // add transition when dark background appear
-        FadeTransition ft = new FadeTransition(Duration.millis(500), vBox);
-        ft.setFromValue(0.0);
-        ft.setToValue(1.0);
-        ft.play();
-
-        dialogBox.showAndWait();
-
-        vBox.setVisible(false);
+        BorderPane borderPane = (BorderPane) ((StackPane) ((Node) actionEvent.getSource()).getScene().getRoot()).getChildren().get(0);
+        Parent pane = loader.load();
+        ChangePasswordController changePasswordController = loader.getController();
+        changePasswordController.initData(user);
+        borderPane.setCenter(pane);
     }
     @FXML private void handleChangeTheme(ActionEvent actionEvent) {
         String newTheme = themeSelector.getValue();
